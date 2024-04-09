b85f471ccb1ba673144ede5689afb721dc9600a3
KahSiong1221 <jiaxiang1221@gmail.com>
2024-04-09 01:55:32 +0100

    sdf

--- src/quantization/onnx_fp32_to_int8.py
+++ src/quantization/onnx_fp32_to_int8.py
@@ -112,7 +112,8 @@ def get_calibration_table(model_path, augmented_model_path, calibration_dataset)
         compute_range = {"L": [0.1, 0.1], "Q": [0.1, 0.1], ......}
     """
     
-    print(calibrator.compute_data())
+    tensors_data = calibrator.compute_data()
+    print(tensors_data.data)
 
     
 
